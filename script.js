10fc29258ab25da31b88811095343a6c0fee3675
ManishYadav <manish2010028@akgec.ac.in>
2021-10-06 01:35:01 +0530

    cloud direction

--- script.js
+++ script.js
@@ -31,17 +31,66 @@ function myMove() {
   function myMove2() {
     let id2 = null;
     const elem2 = document.getElementById("cloud2");   
-    let pos2 = 0;
+    let pos = 0;
  
-    id1 = setInterval(frame1, 15);
-    function frame1() {
-      if (pos1 == 1550) {
+    id2 = setInterval(frame2, 17);
+    function frame2() {
+      if (pos == 1550) {
         clearInterval(id1);
       } else {
-        pos1++;  
-        elem1.style.left = pos1 + "px"; 
+        pos++;  
+        elem2.style.left = pos + "px"; 
+      }
+    }
+  }
+  function myMove3() {
+    let id3 = null;
+    const elem3 = document.getElementById("cloud3");   
+    let pos = 0;
+ 
+    id2 = setInterval(frame3, 14);
+    function frame3() {
+      if (pos == 1550) {
+        clearInterval(id1);
+      } else {
+        pos++;  
+        elem3.style.left = pos + "px"; 
+      }
+    }
+  }
+  function myMove4() {
+    let id4 = null;
+    const elem4 = document.getElementById("cloud4");   
+    let pos = 0;
+ 
+    id4 = setInterval(frame4, 14);
+    function frame4() {
+      if (pos == 1550) {
+        clearInterval(id1);
+      } else {
+        pos++;  
+        elem4.style.left = pos + "px"; 
+      }
+    }
+  }
+  function myMove5() {
+    let id5 = null;
+    const elem5 = document.getElementById("cloud5");   
+    let pos = 0;
+ 
+    id5 = setInterval(frame5, 14);
+    function frame5() {
+      if (pos == 1550) {
+        clearInterval(id5);
+      } else {
+        pos++;  
+        elem5.style.left = pos + "px"; 
       }
     }
   }
   myMove()
   myMove1()
+  myMove2()
+  myMove3()
+  myMove4()
+  myMove5()
