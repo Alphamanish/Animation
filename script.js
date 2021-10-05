6f0ca56cf6009b0dffbdc01479c243db0fcf831e
ManishYadav <manish2010028@akgec.ac.in>
2021-10-05 23:14:27 +0530

    Working on Animation of Cloud and aeroplane

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
-
-
-var imgObj = null;
-var animate ;
-
-function init() {
-   imgObj = document.getElementById('myImage');
-   imgObj.style.position= 'relative'; 
-   imgObj.style.left = '0px'; 
-}
-function moveRight() {
-   imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
-   animate = setTimeout(moveRight,20);    // call moveRight in 20msec
-}
-function stop() {
-   clearTimeout(animate);
-   imgObj.style.left = '0px'; 
-}
-
-window.onload = init;
+function myMove() {
+    let id = null;
+    const elem = document.getElementById("myImage");   
+    let pos = 0;
+ 
+    id = setInterval(frame, 7);
+    function frame() {
+      if (pos == 1550) {
+        clearInterval(id);
+      } else {
+        pos++;  
+        elem.style.left = pos + "px"; 
+      }
+    }
+  }
+  function myMove1() {
+    let id1 = null;
+    const elem1 = document.getElementById("cloud1");   
+    let pos = 0;
+ 
+    id1 = setInterval(frame1, 15);
+    function frame1() {
+      if (pos == 1550) {
+        clearInterval(id1);
+      } else {
+        pos++;  
+        elem1.style.left = pos + "px"; 
+      }
+    }
+  }
+  function myMove2() {
+    let id2 = null;
+    const elem2 = document.getElementById("cloud2");   
+    let pos2 = 0;
+ 
+    id1 = setInterval(frame1, 15);
+    function frame1() {
+      if (pos1 == 1550) {
+        clearInterval(id1);
+      } else {
+        pos1++;  
+        elem1.style.left = pos1 + "px"; 
+      }
+    }
+  }
+  myMove()
+  myMove1()
