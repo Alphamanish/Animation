0f5749dce7435ef3894056d1358202ab453cbd65
ManishYadav <manish2010028@akgec.ac.in>
2021-10-06 09:18:12 +0530

    files added

--- script.js
+++ script.js
@@ -88,28 +88,27 @@ function myMove() {
       }
     }
   }
-//   function myMove6() {
-//     let id6 = null;
-//     const elem6 = document.getElementById("cloud6");   
-//     let pos = 0;
+  function myMove6() {
+    let id6 = null;
+    const elem6 = document.getElementById("cloud6");   
+    let pos = 0;
  
-//     id6 = setTimeout(frame6, 14);
-//     function frame6() {
-//         pos++;  
-//         elem6.style.left = pos + "px"; 
-//       }
-// }
+    id6 = setTimeout(frame6, );
+    function frame6() {
+        pos++;  
+        elem6.style.left = pos + "px"; 
+      }
+  }
 function myMove7() {
     let id7 = null;
     const elem7 = document.getElementById("cloud7");   
     let pos = 0;
- 
-    id7 = setInterval(frame7, 14);
+    id7 = setInterval(frame7, 35);
     function frame7() {
       if (pos == 1550) {
         clearInterval(id7);
       } else {
-        pos++;  
+        pos+= 0.7;
         elem7.style.left = pos + "px";
         elem7.style.bottom = pos + "px"; 
       }
